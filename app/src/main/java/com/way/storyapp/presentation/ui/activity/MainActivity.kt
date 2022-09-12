d7325449bc9b926c0abbffaed5c54574105f512c
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-09-13 00:45:44 +0700

    Move package

--- app/src/main/java/com/way/storyapp/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/way/storyapp/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.way.storyapp
+package com.way.storyapp.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.way.storyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
