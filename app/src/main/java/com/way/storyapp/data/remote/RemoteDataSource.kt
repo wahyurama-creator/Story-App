dafe132da2da94ce44a5e6687746c744db172571
wahyurama-creator <wahyuramadhon32@gmail.com>
2022-09-13 21:57:59 +0700

    Add file param for post story

--- app/src/main/java/com/way/storyapp/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/way/storyapp/data/remote/RemoteDataSource.kt
@@ -4,6 +4,8 @@ import com.way.storyapp.data.remote.model.auth.LoginResponse
 import com.way.storyapp.data.remote.model.auth.PostResponse
 import com.way.storyapp.data.remote.model.auth.UserData
 import com.way.storyapp.data.remote.network.StoryApi
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import javax.inject.Inject
 
@@ -14,8 +16,8 @@ class RemoteDataSource @Inject constructor(private val storyApi: StoryApi) {
     suspend fun login(userData: UserData): Response<LoginResponse> =
         storyApi.login(userData)
 
-    suspend fun postStory(auth: String, parts: Map<String, String>) =
-        storyApi.postStory(auth, parts)
+    suspend fun postStory(auth: String, file: MultipartBody.Part, parts: Map<String, RequestBody>) =
+        storyApi.postStory(auth, file, parts)
 
     suspend fun getAllStory(auth: String, queries: Map<String, String>) =
         storyApi.getAllStory(auth, queries)
